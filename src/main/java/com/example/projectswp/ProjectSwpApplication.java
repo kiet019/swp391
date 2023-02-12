8e944e6adbe2bfa8c43237c21daad010f2efcb32
kiet <kietly019@gmail.com>
2023-02-12 13:29:54 +0700

    new config

--- src/main/java/com/example/projectswp/ProjectSwpApplication.java
+++ src/main/java/com/example/projectswp/ProjectSwpApplication.java
@@ -2,6 +2,9 @@ package com.example.projectswp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProjectSwpApplication {
--- src/main/java/com/example/projectswp/config/WebMvcConfig.java
+++ src/main/java/com/example/projectswp/config/WebMvcConfig.java
@@ -3,15 +3,12 @@ package com.example.projectswp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("*").allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
