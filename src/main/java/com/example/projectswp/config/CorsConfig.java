20cf6967fe4ad05997d375f5584004223b6267c1
kiet <122022327+kiet019@users.noreply.github.com>
2023-02-13 13:41:17 +0700

    Update CorsConfig.java

--- src/main/java/com/example/projectswp/config/CorsConfig.java
+++ src/main/java/com/example/projectswp/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig{
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
