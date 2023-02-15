3bb567783de7efebb56b6c4ab8fc792876e4fe5a
kiet <kietly019@gmail.com>
2023-02-15 22:05:25 +0700

    authentication

--- src/main/java/com/example/projectswp/config/CorsConfig.java
+++ src/main/java/com/example/projectswp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig{
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*").allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
