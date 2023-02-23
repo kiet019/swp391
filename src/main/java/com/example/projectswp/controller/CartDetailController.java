91431f0c5cdd5a222893e03b7ff3bcee31052bd2
BaoDuyLeHoang <90242623+BaoDuyLeHoang@users.noreply.github.com>
2023-02-23 10:28:31 +0700

    Revert "Role"
    This reverts commit 61b2c2eb1972f6fa4d030e01c02acf259781bf91.

--- src/main/java/com/example/projectswp/controller/CartDetailController.java
+++ src/main/java/com/example/projectswp/controller/CartDetailController.java
@@ -7,7 +7,6 @@ import com.example.projectswp.repositories.CartDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,24 +32,23 @@ public class CartDetailController {
         return cartDetails != null ? ResponseEntity.ok(cartDetails) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @PostMapping("/create")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("")
     public ResponseEntity<CartDetails> createCartDetail(@RequestBody CartDetails addCartDetails) {
         boolean result = cartDetailsRepository.addCartDetails(addCartDetails);
         URI uri = URI.create("localhost:8080/api/cartdetail/" + cartDetailsRepository.getLastCartDetails().getCartDetailsID());
         return result ? ResponseEntity.created(uri).build() : ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
     }
 
-//    @PutMapping("/{cartDetailID}")
-//    public ResponseEntity<CartDetails> updateCartDetail(@PathVariable int cartDetailID, @RequestBody CartDetails cartDetail) {
-//        boolean result = cartDetailsRepository.updateCartDetail(cartDetailID, cartDetail);
-//        return result ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//    }
-//    @DeleteMapping("/{cartDetailID}")
-//    public ResponseEntity<CartDetails> deleteCartDetail(@PathVariable int cartDetailID){
-//        boolean result = cartDetailsRepository.deleteCartDetail(cartDetailID);
-//        return result ? ResponseEntity.accepted().build() : ResponseEntity.notFound().build();
-//    }
+    @PutMapping("/{cartDetailID}")
+    public ResponseEntity<CartDetails> updateCartDetail(@PathVariable int cartDetailID, @RequestBody CartDetails cartDetail) {
+        boolean result = cartDetailsRepository.updateCartDetail(cartDetailID, cartDetail);
+        return result ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    @DeleteMapping("/{cartDetailID}")
+    public ResponseEntity<CartDetails> deleteCartDetail(@PathVariable int cartDetailID){
+        boolean result = cartDetailsRepository.deleteCartDetail(cartDetailID);
+        return result ? ResponseEntity.accepted().build() : ResponseEntity.notFound().build();
+    }
     @PatchMapping("/accept")
     public void accept(@RequestBody int cartDetailId ){
 
