1ad1f7bef2d33d1d1520d30c046a2cb21ca671fb
kiet019 <kietly019@gmail.com>
2023-03-10 09:32:06 +0700

    getting send email

--- src/main/java/com/example/projectswp/controller/CategoryController.java
+++ src/main/java/com/example/projectswp/controller/CategoryController.java
@@ -2,7 +2,6 @@ package com.example.projectswp.controller;
 
 import com.example.projectswp.model.Category;
 import com.example.projectswp.repositories.CategoryRepository;
-import com.example.projectswp.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
